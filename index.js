3efbcbf419a5cb15ff99a6ed967f64d9407527fd
Taras Kucherenko <t.kucherenko@binarysages.com>
2021-01-19 18:22:33 +0200

    feat: add action for the game ☠

--- index.js
+++ index.js
@@ -2,34 +2,96 @@ const characterName = 'Eritarhero';
 const characterClass = 'Warrior';
 let characterLocation = 'Elven Forest';
 let characterHealth = 168;
-let characterDamage = 4;
-
-const mobName = 'Ogre';
-const mobClass = 'Mage';
-const mobHealth = 124;
-const mobDamage = 2;
+let characterDamage = 26;
 
 let isCharacterDead = false;
 let isAlliancePlayerNear  = false;
 
-console.log(`
-  You: ${characterName}
-  Class: ${characterClass}
-  Health: ${characterHealth} hp
-  Damage: ${characterDamage} dmg
-  Location: ${characterLocation}
-  || ============================
-  Enemy: ${mobName}
-  Class: ${mobClass}
-  Health: ${mobHealth} hp
-  Damage: ${mobDamage} dmg
-  || ============================
-  || Fight
-  || ============================
-  || ----------------------------
-  ${mobName} ${mobClass} hit ${characterName} with ${mobDamage} dmg
-  ${characterName} Health: ${characterHealth - mobDamage}
-  || ----------------------------
-  ${characterName} hit ${mobName} ${mobClass} with ${mobDamage} dmg
-  ${mobName} ${mobClass} Health: ${mobHealth - characterDamage}
+const mobName = 'Ogre';
+const mobClass = 'Mage';
+let mobHealth = 124;
+const mobDamage = 19;
+let isMobDead = false;
+
+const deadText = 'dead  ☠ ☠';
+
+function fight() {
+  console.log(`
+|| ============================
+|| Fight
+|| ============================`)
+  for (let i = 1; characterHealth > 0 && mobHealth > 0; i++) {
+    characterHealth -= mobDamage;
+    mobHealth -= characterDamage;
+
+    if (characterHealth < 0) {
+      characterHealth = 0;
+    }
+
+    if (mobHealth < 0) {
+      mobHealth = 0;
+    }
+
+    if (characterHealth === 0 && mobHealth > 0) {
+      isCharacterDead = true
+    }
+
+    if (characterHealth > 0 && mobHealth === 0) {
+      isMobDead = true
+    }
+
+    if (characterHealth === 0 && mobHealth === 0) {
+      isCharacterDead = true
+      isMobDead = true
+    }
+
+    console.log(`
+=== Round ${i} ============================================
 `)
+
+    if (characterHealth > 0 && mobHealth > 0) {
+      console.log(`${mobName} ${mobClass} hit ${characterName} with ${mobDamage} dmg
+${characterName} Health: ${characterHealth}
+
+|| ----------------------------
+
+${characterName} hit ${mobName} ${mobClass} with ${characterDamage} dmg
+${mobName} ${mobClass} Health: ${mobHealth}`)
+    }
+
+
+  }
+
+  if (isCharacterDead) {
+    console.log(`
+☠ ☠  ${characterClass} ${characterName} is ${deadText}
+    `)
+  }
+
+  if (isMobDead) {
+    console.log(`
+☠ ☠  ${mobName} ${mobClass} is ${deadText}
+    `)
+  }
+
+  if (isCharacterDead && isMobDead) {
+    console.log(`
+☠ ☠  ${characterClass} ${characterName} and ${mobName} ${mobClass} are ${deadText}
+    `)
+  }
+}
+
+
+console.log(`
+You: ${characterName}
+Class: ${characterClass}
+Health: ${characterHealth} hp
+Damage: ${characterDamage} dmg
+Location: ${characterLocation}
+|| ============================
+Enemy: ${mobName}
+Class: ${mobClass}
+Health: ${mobHealth} hp
+Damage: ${mobDamage} dmg`)
+
+fight()
